1f3620aed3323c44433169e726cc482993b6190d
YuriiTrofimenko <tyaa@ukr.net>
2023-10-31 16:23:43 +0300

    init

---
+++ src/test/java/org/tyaa/training/server/TrainingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyaa.training.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
