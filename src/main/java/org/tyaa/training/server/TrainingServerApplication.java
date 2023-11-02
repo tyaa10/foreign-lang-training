db3761e5580a064682678921048bb8beb2580ac7
YuriiTrofimenko <tyaa@ukr.net>
2023-11-02 15:31:27 +0300

    инициализация базы данных: роли

--- src/main/java/org/tyaa/training/server/TrainingServerApplication.java
+++ src/main/java/org/tyaa/training/server/TrainingServerApplication.java
@@ -1,15 +1,37 @@
 package org.tyaa.training.server;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.tyaa.training.server.entities.RoleEntity;
+import org.tyaa.training.server.repositories.RoleRepository;
+
+import java.util.List;
 
 /**
  * Главный класс приложения
  * */
 @SpringBootApplication
 public class TrainingServerApplication {
+
+	@Value("${custom.init-data.roles}")
+	private List<String> roles;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingServerApplication.class, args);
 	}
 
+	/**
+	 * Инициализация БД
+	 * */
+	@Bean
+	public CommandLineRunner initData (RoleRepository roleRepository) {
+		return args -> {
+			roleRepository.truncateTable();
+			roleRepository.save(RoleEntity.builder().name("ROLE_ADMIN").build());
+			roleRepository.save(RoleEntity.builder().name("ROLE_CUSTOMER").build());
+		};
+	}
 }
--- src/main/java/org/tyaa/training/server/repositories/RoleRepository.java
+++ src/main/java/org/tyaa/training/server/repositories/RoleRepository.java
@@ -1,6 +1,9 @@
 package org.tyaa.training.server.repositories;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.tyaa.training.server.entities.RoleEntity;
 
@@ -9,4 +12,8 @@ import org.tyaa.training.server.entities.RoleEntity;
  * */
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+    @Modifying
+    @Transactional
+    @Query(value = "TRUNCATE TABLE Roles RESTART IDENTITY", nativeQuery = true)
+    void truncateTable();
 }
